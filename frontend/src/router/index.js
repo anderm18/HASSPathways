f4746409d6f7f9a248381bd72a171395056d41f9
kharnkharn <156844580+kharnkharn@users.noreply.github.com>
2024-04-12 16:25:40 -0400

    Changed index.js to include Search page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,6 +9,7 @@ import FromClassesPage from '../pages/FromClasses/FromClassesPage.vue'
 import FromClassesPathways from '../pages/FromClasses/FromClassesPathways.vue'
 import AboutPage from '../pages/About/AboutPage.vue'
 import AdvancedSearchPage from '../pages/AdvancedSearch/AdvancedSearchPage.vue'
+import Search from '../pages/Search/Search.vue'
 import CoursesPage from '../pages/Courses/CoursesPage.vue'
 import CoursePage from '../pages/Courses/CoursePage.vue'
 // import AdminHomePage from '../pages/AdminPortal/AdminHomePage.vue'
@@ -60,6 +61,11 @@ export default new Router({
             path: '/search-classes',
             name: 'search-classes',
             component: AdvancedSearchPage
+        },
+        {
+            path: '/search',
+            name: 'search',
+            component: Search
         },
         {
             path: '/courses',
